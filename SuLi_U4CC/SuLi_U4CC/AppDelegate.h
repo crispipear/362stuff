d0638cc59bff2de4f4e81844aaac10b6be7500ea
Su Li <rice74@D-10-156-31-57.dhcp4.washington.edu>
2018-02-08 14:58:05 -0800

    U4 CC

---
+++ SuLi_U4CC/SuLi_U4CC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuLi_U4CC
+//
+//  Created by Su Li on 2/8/18.
+//  Copyright © 2018 Su Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
