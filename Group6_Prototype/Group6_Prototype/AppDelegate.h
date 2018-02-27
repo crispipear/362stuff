7f0e8c550afc55dce256fccc9a794ca15d75ed0b
Su Li <rice74@D-10-156-29-219.dhcp4.washington.edu>
2018-02-27 14:47:28 -0800

    prototype fix button after segue action

---
+++ Group6_Prototype/Group6_Prototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Group6_Prototype
+//
+//  Created by Su Li on 2/13/18.
+//  Copyright © 2018 Su Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Group6_Prototype/Group6_Prototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Group6_Prototype
+//
+//  Created by Su Li on 2/13/18.
+//  Copyright © 2018 Su Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
